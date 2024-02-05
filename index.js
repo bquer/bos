4f763aa0db33801c1824a944ab84636c153284af
bquer <83635236+bquer@users.noreply.github.com>
2024-02-05 16:21:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ const app = express();
 const client = new PrivateBinClient("https://privatebin.net/");
 
 app.use(bodyParser.json());
-app.post('/upload', async (req, res) => {
+app.get('/upload', async (req, res) => {
     try {
-        const content = req.body.data; 
+        const content = req.query.data; 
         const result = await client.uploadContent(content, { uploadFormat: 'markdown', expiry: '5min' });
         const id = result.response.id;
         const urlKey = result.urlKey;
         const baseUrl = "https://privatebin.net/?";
         const url = `${baseUrl}${id}#${urlKey}`;
-        res.send(`${url}`); 
+        res.send(`${url}`);
     } catch (error) {
         console.error("Bir hata oluştu:", error);
-        res.status(500).send("Internal Server Error"); 
+        res.status(500).send("Internal Server Error");
     }
 });
+
 
 app.listen(80)
